185368f397119fd0df6beb3a57732178d6b34827
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-11-13 01:36:55 -0800

    Done with percolates() and numberOfOpenSites() function but still need to find a efficient way to implement isFull() function.

--- 1_5/cod_p1/Percolation.java
+++ 1_5/cod_p1/Percolation.java
@@ -1,27 +1,25 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.lang.IllegalArgumentException;
 
 public class Percolation {
-	private boolean[] sites;
-	private WeightedQuickUnionUF a_union_set ;
-	private int size;
+	
 	public Percolation(int n) {
+
+		int last_row_first_col = n * (n - 1);
+		opened = 0;
+
 		sites = new boolean[n];
 		//Resever 2 nodes for top and bottom
 		a_union_set = new WeightedQuickUnionUF(n*n + 2);
-		int top = n*n;
-		int bottom = n*n + 1;
-		int last_row_first_col = n * (n - 1);
+		top = n*n;
+		bottom = n*n + 1;
 		
 		//Connect 1st row to top node and connect last row to bot node
 		for(int i = 0; i < n; i++) {
 			a_union_set.connected(i, top);
 			a_union_set.connected(last_row_first_col + i, bottom);
 		}
-		size = n;
-		
+		size = n;		
 		
 	}
 	
@@ -31,6 +29,7 @@ public class Percolation {
 		int[] neigh = {-1, -1, -1, -1};
 		
 		sites[i] = true;
+		opened++;
 		//Get upper elem
 		if(row != 1) neigh[0] = (row - 2) * size + col - 1;
 		//Get right elem
@@ -43,6 +42,7 @@ public class Percolation {
 		for(int e : neigh) {
 			if(e != -1 && sites[e]) a_union_set.connected(i, e);
 		}
+		
 	}
 	
 	public boolean isOpen(int row, int col) {
@@ -51,4 +51,19 @@ public class Percolation {
 		return sites[i];
 	}
 	
+	public int numberOfOpenSites() {
+		return opened;
+	}
+	
+	public boolean percolates() {
+		return a_union_set.connected(top, bottom);
+	}
+	
+
+	private boolean[] sites;
+	private WeightedQuickUnionUF a_union_set ;
+	private int size;
+	private int top;
+	private int bottom;
+	private int opened;
 }
