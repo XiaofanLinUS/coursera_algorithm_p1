3ca217ca38255dc248db084c94d84eb6d1c0a6e1
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-11-14 21:52:39 -0800

    Change the implementation on random sites from using uniform(), a.k.a. random(), on index to using shuffle() on ordered numbers.

--- 1_5/cod_p1/PercolationStats.java
+++ 1_5/cod_p1/PercolationStats.java
@@ -67,3 +67,83 @@ public class PercolationStats {
 	private double mean, stddev, confidenceLo, confidenceHi;
 }
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+	public PercolationStats(int n, int trials) {
+
+		int p;
+		int col = -1, row = -1;
+		int size = n * n;
+		int[] randomSites = new int[size];
+		
+		t = trials;
+		//How many successes are needed to perolate
+		success = new double[trials];
+		
+		mean = -1;
+		stddev = -1;
+		confidenceHi = -1;
+		confidenceLo = -1;
+		
+		for(int i = 0; i < size; i++) {
+			randomSites[i] = i;
+		}
+		
+		for(int i = 0; i < trials; i++) {
+
+			//Reintialize index p and grid
+			//Shuffle the random sites
+			
+			p = 0;
+			grid = new Percolation(n);
+			StdRandom.shuffle(randomSites);
+			
+			while(!grid.percolates()) {
+				success[i]++;
+				
+				row = randomSites[p] / n + 1;
+				col = randomSites[p] % n + 1;
+				grid.open(row, col);
+				p++;
+			}
+			success[i] /= size;
+		}
+	}
+	
+	public double mean() {
+		if(mean == -1.0) mean = StdStats.mean(success);
+		return mean;
+	}
+	
+	public double stddev() {
+		if(stddev == -1) stddev = StdStats.stddev(success);
+		return stddev;
+	}
+	
+	public double confidenceLo() {
+		if(confidenceLo == -1) confidenceLo = mean() - 1.96 * stddev() / Math.sqrt(t);
+		return confidenceLo;
+	}
+	
+
+	public double confidenceHi() {
+		if(confidenceHi == -1) confidenceHi = mean() + 1.96 * stddev() / Math.sqrt(t);
+		return confidenceLo;
+	}
+	
+	
+	public static void main(String[] args) {
+		PercolationStats test = new PercolationStats(200, 10000);
+		System.out.println(test.mean());
+		
+	}
+	
+	
+	private Percolation grid;
+	private int t;
+	private double[] success;
+	private double mean, stddev, confidenceLo, confidenceHi;
+}
