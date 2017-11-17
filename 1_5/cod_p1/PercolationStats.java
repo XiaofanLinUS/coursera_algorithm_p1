8917c222a37a39878ea977ba99bf8c3d9c6eb283
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-11-16 21:05:32 -0800

    toString() added

--- 1_5/cod_p1/PercolationStats.java
+++ 1_5/cod_p1/PercolationStats.java
@@ -5,75 +5,6 @@ public class PercolationStats {
 
 	public PercolationStats(int n, int trials) {
 
-		int p = -1;
-		int col = -1, row = -1;
-		int size = n * n;
-		
-		t = trials;
-		success = new double[trials];
-		
-		mean = -1;
-		stddev = -1;
-		confidenceHi = -1;
-		confidenceLo = -1;
-		for(int i = 0; i < trials; i++) {
-
-			grid = new Percolation(n);
-			while(!grid.percolates()) {
-				success[i]++;
-				
-				p = (int) (StdRandom.uniform() * size);
-				row = p / n + 1;
-				col = p % n + 1;
-				grid.open(row, col);
-			}
-			success[i] /= size;
-			
-		}
-	}
-	
-	public double mean() {
-		if(mean == -1.0) mean = StdStats.mean(success);
-		return mean;
-	}
-	
-	public double stddev() {
-		if(stddev == -1) stddev = StdStats.stddev(success);
-		return stddev;
-	}
-	
-	public double confidenceLo() {
-		if(confidenceLo == -1) confidenceLo = mean() - 1.96 * stddev() / Math.sqrt(t);
-		return confidenceLo;
-	}
-	
-
-	public double confidenceHi() {
-		if(confidenceHi == -1) confidenceHi = mean() + 1.96 * stddev() / Math.sqrt(t);
-		return confidenceLo;
-	}
-	
-	
-	public static void main(String[] args) {
-		PercolationStats test = new PercolationStats(200, 100);
-		System.out.println(test.mean());
-		
-	}
-	
-	
-	private Percolation grid;
-	private int t;
-	private double[] success;
-	private double mean, stddev, confidenceLo, confidenceHi;
-}
-
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-
-public class PercolationStats {
-
-	public PercolationStats(int n, int trials) {
-
 		int p;
 		int col = -1, row = -1;
 		int size = n * n;
@@ -119,7 +50,7 @@ public class PercolationStats {
 	}
 	
 	public double stddev() {
-		if(stddev == -1) stddev = StdStats.stddev(success);
+		if(stddev == -1.0) stddev = StdStats.stddev(success);
 		return stddev;
 	}
 	
@@ -131,14 +62,25 @@ public class PercolationStats {
 
 	public double confidenceHi() {
 		if(confidenceHi == -1) confidenceHi = mean() + 1.96 * stddev() / Math.sqrt(t);
-		return confidenceLo;
+		return confidenceHi;
 	}
 	
 	
 	public static void main(String[] args) {
-		PercolationStats test = new PercolationStats(200, 10000);
-		System.out.println(test.mean());
+		PercolationStats test = new PercolationStats(200, 100);
+		System.out.println(test);
 		
+	}
+	
+	public String toString() {
+		String a = "mean                    = ";
+		a += this.mean() + "\n";
+		a += "stddev                  = ";
+		a += this.stddev() + "\n";
+		a += "95% confidence interval = [";
+		a += this.confidenceLo() + ", " + this.confidenceHi() + "]\n";
+		
+		return a;
 	}
 	
 	
