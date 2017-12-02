e5c9a1b98d0701c5b2978ed2a48c5d5e7e7f07ca
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-12-02 01:16:27 -0800

    That was not linear. This is linear!

--- 1_4/int_p1/LinearSum.java
+++ 1_4/int_p1/LinearSum.java
@@ -2,22 +2,21 @@ public class LinearSum {
 
 	public static void main(String[] args) {
 		int[] nums = { 10, 20, 30, 40, 50 };
-		int n = countSums(nums, 20);
+		int n = countSums(nums, 70);
 		System.out.println(n);
 	}
 
 	public static int countSums(int[] a, int goal) {
 		int count = 0;
-		int i = 0, j = 1;
-		while (i != j && j < a.length) {
-			if (a[i] + a[j] < goal) {
-				j++;
-			} else if (a[i] + a[j] > goal) {
-				i++;
+		int i = 0, j = a.length - 1;
+		while (i != j) {
+			if (a[i] + a[j] > goal) {
+				j--;
+			} else if (a[i] + a[j] < goal) {
+				break;
 			} else {
 				count++;
 				i++;
-				j = i+1;
 			}
 		}
 		return count;
