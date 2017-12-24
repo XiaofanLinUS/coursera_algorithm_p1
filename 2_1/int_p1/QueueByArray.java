c8deb59dc0c6dce2cc3a008f616b26c2a7a86621
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-12-24 15:40:38 -0800

    working on queue by resizable array

---
+++ 2_1/int_p1/QueueByArray.java
@@ -0,0 +1,64 @@
+class QueueByArray<Item> {
+	private Item[] items;
+	private int head, tail;
+	private final int ScaleFactor = 2;
+
+    	public static void main(String[] args) {
+	    QueueByArray<String> q = new QueueByArray<>();
+
+	    for(int i = 0; i < 50; i++) {
+		q.enqueue(Integer.toString(i));
+		Object[] stuffs = (Object[]) q.items;		
+		System.out.println("Size: " + stuffs.length);
+	    }
+
+
+	    int size = q.tail - q.head;
+	    for(int i = 0; i < size; i++) {
+		System.out.println(q.dequeue());
+		Object[] stuffs = (Object[]) q.items;		
+		System.out.println("Size: " + stuffs.length);
+
+	    }
+
+	}
+
+	public QueueByArray() {
+		items = (Item[]) new Object[1];
+		head = 0;
+		tail = 0;
+	}
+
+	public void enqueue(Item i) {
+		items[tail++] = i;
+		if (tail >= items.length)
+			adjust();
+	}
+
+	public Item dequeue() {
+		Item theItem = items[head++];
+		if (head >= items.length * 3 / 4)
+			adjust();
+		return theItem;
+	}
+
+	private void adjust() {
+		Object[] newPlace;
+
+		int size = tail - head;
+		if (size >= items.length) {
+			newPlace = new Object[items.length * ScaleFactor];
+		} else if (size <= items.length / (ScaleFactor * ScaleFactor)) {
+			newPlace = new Object[items.length / ScaleFactor];
+		} else {
+			newPlace = new Object[items.length];
+		}
+		head -= head;
+		tail -= head;
+
+		for (int i = 0; i < tail; i++) {
+			newPlace[i] = items[i];
+		}
+		items = (Item[]) newPlace;
+	}
+}
