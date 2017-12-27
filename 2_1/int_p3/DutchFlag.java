81ffae3d564208bbfb5d3828255b7b40ac16ae26
Xiaofan Lin <xiaofanlinus@gmail.com>
2017-12-26 22:54:00 -0800

    Tough problem huhgit add DutchFlag.java git add DutchFlag.java  Fixed!

--- 2_1/int_p3/DutchFlag.java
+++ 2_1/int_p3/DutchFlag.java
@@ -2,8 +2,9 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class DutchFlag {
-	private int[] buckets;
-	private int size;
+	private int[] buckets = { 2, 0, 1, 2, 1, 2, 0, 0, 2, 1 };
+
+	private int size = 10;
 
 	public DutchFlag(int numb) {
 		size = numb;
@@ -16,24 +17,36 @@ public class DutchFlag {
 
 	}
 
+	public DutchFlag() {
+
+	}
+
 	public void sort() {
 		int i = 0;
 		int j = size - 1;
 		int k = 0;
-		while (k < j) {
+
+		/**
+		 * From 0 to i - 1, buckets contain redballs. From i to k - 1, buckets contain
+		 * whiteballs. From k to j - 1, buckets are unexamined. From j - 1 to size - 1,
+		 * buckets contain blueballs.
+		 **/
+
+		while (k <= j) {
 			if (buckets[k] == 0) {
 				swap(i, k);
 				i++;
 				k++;
-			}
+			} else
+
 			if (buckets[k] == 1) {
 				k++;
-			}
+			} else
+
 			if (buckets[k] == 2) {
 				swap(j, k);
 				j--;
 			}
-
 		}
 
 	}
@@ -45,7 +58,7 @@ public class DutchFlag {
 	}
 
 	public static void main(String[] args) {
-		DutchFlag df = new DutchFlag(5);
+		DutchFlag df = new DutchFlag();
 		df.sort();
 		StdOut.println("Sorted: ");
 		for (int e : df.buckets) {
